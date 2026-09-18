5f21a3bec71643045927bb926b3deb82c84e7452
mrramyaoun-crypto <mr.ramy.aoun@gmail.com>
2026-09-18 14:18:37 +0300

    Bump redesigned clue build

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -10,7 +10,7 @@ android {
         applicationId = "com.codebreaker.game"
         minSdk = 24
         targetSdk = 35
-        versionCode = 13
-        versionName = "11.0"
+        versionCode = 14
+        versionName = "12.0"
     }
 }
