8baa1badae0a18eb153ac9ee8394013732f882b0
mrramyaoun-crypto <mr.ramy.aoun@gmail.com>
2026-09-18 10:49:14 +0300

    Add Code Breaker Android app

---
+++ app/build.gradle.kts
@@ -0,0 +1,16 @@
+plugins {
+    id("com.android.application")
+}
+
+android {
+    namespace = "com.codebreaker.game"
+    compileSdk = 35
+
+    defaultConfig {
+        applicationId = "com.codebreaker.game"
+        minSdk = 24
+        targetSdk = 35
+        versionCode = 3
+        versionName = "3.0"
+    }
+}
