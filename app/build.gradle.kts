03ed0f4ca8a39a124ca3e9ee0456a5549e1136bd
mrramyaoun-crypto <mr.ramy.aoun@gmail.com>
2026-09-18 11:34:24 +0300

    Bump Code Breaker options-board build

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -10,7 +10,7 @@ android {
         applicationId = "com.codebreaker.game"
         minSdk = 24
         targetSdk = 35
-        versionCode = 6
-        versionName = "4.0"
+        versionCode = 7
+        versionName = "5.0"
     }
 }
