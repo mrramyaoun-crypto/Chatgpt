c55dabdba5ed6deb740930e06c2cef0604c268a2
mrramyaoun-crypto <mr.ramy.aoun@gmail.com>
2026-09-18 13:27:00 +0300

    Bump Enigma worn-machine build

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -10,7 +10,7 @@ android {
         applicationId = "com.codebreaker.game"
         minSdk = 24
         targetSdk = 35
-        versionCode = 10
-        versionName = "8.0"
+        versionCode = 11
+        versionName = "9.0"
     }
 }
